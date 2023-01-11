ec108fb42e68fdfd31113dd26ba89a54742fbee1
AudreyEnamhe <audreyenamhe28@gmail.com>
2023-01-10 23:52:48 -0800

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 {
 	char *p;
 	unsigned int i = 0;
-	
+
 	if (size == 0)
 		return (NULL);
 	p = (char *) malloc(sizeof(char) * size);
@@ -24,6 +24,6 @@ char *create_array(unsigned int size, char c)
 		i++;
 	}
 	*(p + i) = '\0';
-	
+
 	return (p);
 }
